f72236bba0637fa60b5631acde734035f4c7206e
Igor <igor.fizyk@gmail.com>
2023-08-20 12:42:46 +0200

    work with adding questions to questionArrayList

--- src/main/java/sda/groupProject/knowledgeChecker/data/JSONConnector.java
+++ src/main/java/sda/groupProject/knowledgeChecker/data/JSONConnector.java
@@ -8,30 +8,82 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class JSONConnector {
-    public static ArrayList<Question> questionArrayList = new ArrayList<>();
+    public List<Question> questionArrayList = new ArrayList<>();
     public JSONConnector() throws IOException {
 
         URL questionsJsonURL = new URL("https://public.andret.eu/questions.json");
         HttpURLConnection connection = (HttpURLConnection) questionsJsonURL.openConnection();
-        connection.setRequestMethod("GET");
+//        connection.setRequestMethod("GET");
         // int responseCode = connection.getResponseCode();
-
         JSONTokener jsonTokener = new JSONTokener(connection.getInputStream());
-
-
         JSONArray jsonArray = new JSONArray(jsonTokener);
+
+
+        int idForCategory = 1;
+        Map<String, Integer> categoryMap = new HashMap<>();
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            String categoryName = jsonObject.getString("category");
+            if (!categoryMap.containsKey(categoryName)) {
+                categoryMap.put(categoryName, idForCategory++);
+            }
+        }
 
         for (int i = 0; i < jsonArray.length(); i++) {
             JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+            Advancement advancement = (jsonObject.getString("advancement").equals("medium"))
+                    ? Advancement.MEDIUM :
+                    (jsonObject.getString("advancement").equals("basic"))
+                    ? Advancement.BASIC : Advancement.EXPERT;
+
+
+            JSONArray JSONArrayAnswers = jsonObject.getJSONArray("answers");
+            List<Answer> listOfAnswers = new ArrayList<>();
+            //add new question
+            for (int j = 0; j < JSONArrayAnswers.length(); j++) {
+
+                JSONObject
+
+                String text = ;
+                boolean correct = ;
+                String explanation = ;
+
+
+                listOfAnswers.add(new Answer(text, correct, explanation))
+            }
+
+
+            String categoryName = jsonObject.getString("category");
+            Category questionCategory =
+                    new Category(categoryName, categoryMap.get(categoryName));
+
+
             questionArrayList.add(new Question(
                     jsonObject.getInt("id"),
-                    jsonObject.getString("advancement"),
-                    //new Category("category"),
+                    advancement,
+                    questionCategory,
                     jsonObject.getString("text"),
-                    jsonObject.getString("answers"),
-            ))
+                    listOfAnswers));
         }
     }
+//    int setIdCategory(String categoryName) {
+//         if (questionArrayList.stream()
+//                .anyMatch(question -> question.category().getCategoryName().equals(categoryName))) {
+//             for (Question question : questionArrayList) {
+//                 if (question.category().getCategoryName().equals(categoryName)) {
+//                     return question.category().getCategoryId();
+//                 }
+//             }
+//         } else {
+//             idForCategory++;
+//             return idForCategory;
+//         };
+//          return  0;
+//    }
 }
--- src/main/java/sda/groupProject/knowledgeChecker/data/Question.java
+++ src/main/java/sda/groupProject/knowledgeChecker/data/Question.java
@@ -1,61 +1,8 @@
 package sda.groupProject.knowledgeChecker.data;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-public class Question {
+import java.util.List;
 
-    private int id;
-    private Advancement advancement;
-    private Category category;
-    private String text;
-    private Answer[] answers;
+public record Question(int id, Advancement advancement, Category category, String text,
+                       List <Answer> answers) {
 
-    public Question(int id, Advancement advancement, Category category, String text, Answer[] answers) {
-        this.id = id;
-        this.advancement = advancement;
-        this.category = category;
-        this.text = text;
-        this.answers = answers;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public Advancement getAdvancement() {
-        return advancement;
-    }
-
-    public void setAdvancement(Advancement advancement) {
-        this.advancement = advancement;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public Answer[] getAnswers() {
-        return answers;
-    }
-
-    public void setAnswers(Answer[] answers) {
-        this.answers = answers;
-    }
 }
