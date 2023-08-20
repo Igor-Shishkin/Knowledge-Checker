61fb8b5cc4744a4250a665f2cfc35c9499f04357
ArturPososhko <44324534+ArturPososhko@users.noreply.github.com>
2023-08-20 15:19:31 +0200

    Updated JSONConnector logic and added comments explaining the logic, converted Answer and Category classes to records

--- src/main/java/sda/groupProject/knowledgeChecker/data/Category.java
+++ src/main/java/sda/groupProject/knowledgeChecker/data/Category.java
@@ -1,28 +1,7 @@
 package sda.groupProject.knowledgeChecker.data;
 
-public class Category {
-    private int categoryId;
-    private String categoryName;
+public record Category(String categoryName,
+                       int categoryId) {
 
-    public Category(String categoryName, int categoryId) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
-
-    public int getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(int categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName() {
-        this.categoryName = categoryName;
-    }
 
 }
--- src/main/java/sda/groupProject/knowledgeChecker/data/JSONConnector.java
+++ src/main/java/sda/groupProject/knowledgeChecker/data/JSONConnector.java
@@ -15,15 +15,15 @@ import java.util.Map;
 public class JSONConnector {
     public List<Question> questionArrayList = new ArrayList<>();
     public JSONConnector() throws IOException {
-
+        // Creating a jsonArray from https://public.andret.eu/questions.json
         URL questionsJsonURL = new URL("https://public.andret.eu/questions.json");
         HttpURLConnection connection = (HttpURLConnection) questionsJsonURL.openConnection();
-//        connection.setRequestMethod("GET");
-        // int responseCode = connection.getResponseCode();
+            //connection.setRequestMethod("GET");
+            //int responseCode = connection.getResponseCode();
         JSONTokener jsonTokener = new JSONTokener(connection.getInputStream());
         JSONArray jsonArray = new JSONArray(jsonTokener);
 
-
+        // Iterating through JSONArray to assign ID's to each unique category value.
         int idForCategory = 1;
         Map<String, Integer> categoryMap = new HashMap<>();
         for (int i = 0; i < jsonArray.length(); i++) {
@@ -34,42 +34,49 @@ public class JSONConnector {
             }
         }
 
+        // Iterating through JSONArray again to ...
         for (int i = 0; i < jsonArray.length(); i++) {
+            // creating a JSONObject with key-value pairs from JSONArray
             JSONObject jsonObject = jsonArray.getJSONObject(i);
 
-            Advancement advancement = (jsonObject.getString("advancement").equals("medium"))
+            // creating an int variable and assigning value of that JSONObject's "id" key-value pair
+            int questionID = jsonObject.getInt("id");
+
+            // creating an enum object of type Advancement and assigning value of that JSONObject's "advancement" key-value pair
+            Advancement questionAdvancement = (jsonObject.getString("advancement").equals("medium"))
                     ? Advancement.MEDIUM :
                     (jsonObject.getString("advancement").equals("basic"))
                     ? Advancement.BASIC : Advancement.EXPERT;
 
+            // creating an object of type Category and assigning value of that JSONObject's "category" key-value pair
+            String categoryName = jsonObject.getString("category");
+            Category questionCategory = new Category(categoryName, categoryMap.get(categoryName));
 
+            // creating an object of type String and assigning value of that JSONObject's "text" key-value pair
+            String questionText = jsonObject.getString("text");
+
+            // creating an array and assigning values of that JSONObject's "answers" key-value pair
             JSONArray JSONArrayAnswers = jsonObject.getJSONArray("answers");
-            List<Answer> listOfAnswers = new ArrayList<>();
-            //add new question
+            // creating a list of class type Answer
+            List<Answer> questionListOfAnswers = new ArrayList<>();
+            // Iterating through that created array
             for (int j = 0; j < JSONArrayAnswers.length(); j++) {
-
-                JSONObject
-
-                String text = ;
-                boolean correct = ;
-                String explanation = ;
-
-
-                listOfAnswers.add(new Answer(text, correct, explanation))
+                // creating a JSONObject with key-value pairs for each entry in that array
+                JSONObject jsonAnswerObject = JSONArrayAnswers.getJSONObject(i);
+                // assigning values of newly created object's "text", "correct", and "explanation" key-value pairs
+                String text = jsonAnswerObject.getString("text");
+                boolean correct = jsonAnswerObject.getBoolean("correct");
+                String explanation = jsonAnswerObject.getString("explanation");
+                // creating an object of type Answer with retrieved values and adding that object to before created list
+                questionListOfAnswers.add(new Answer(text, correct, explanation));
             }
 
-
-            String categoryName = jsonObject.getString("category");
-            Category questionCategory =
-                    new Category(categoryName, categoryMap.get(categoryName));
-
-
             questionArrayList.add(new Question(
-                    jsonObject.getInt("id"),
-                    advancement,
+                    questionID,
+                    questionAdvancement,
                     questionCategory,
-                    jsonObject.getString("text"),
-                    listOfAnswers));
+                    questionText,
+                    questionListOfAnswers));
         }
     }
 //    int setIdCategory(String categoryName) {
