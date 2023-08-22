718c1cd1d85a493f677eaca216496162d5393eb7
ArturPososhko <44324534+ArturPososhko@users.noreply.github.com>
2023-08-22 21:05:32 +0200

    Added changeTextToHTML method

---
+++ src/main/java/sda/groupProject/knowledgeChecker/data/HTMLConverter.java
@@ -0,0 +1,33 @@
+package sda.groupProject.knowledgeChecker.data;
+
+public class HTMLConverter {
+    public static String changeTextToHTML(String text, int lineLength) {
+        if (text == null || text.isEmpty() || lineLength <= 0) {
+            return "";
+        }
+
+        StringBuilder result = new StringBuilder("<html>");
+        int startIndex = 0;
+
+        while (startIndex < text.length()) {
+            int endIndex = Math.min(startIndex + lineLength, text.length());
+            String chunk = text.substring(startIndex, endIndex);
+
+            if (endIndex < text.length()) {
+                int lastSpaceIndex = chunk.lastIndexOf(' ');
+
+                if (lastSpaceIndex != -1) {
+                    endIndex = startIndex + lastSpaceIndex;
+                    chunk = chunk.substring(0, lastSpaceIndex);
+                }
+            }
+
+            result.append(chunk).append("<br>");
+            startIndex = endIndex + 1;
+        }
+
+        result.append("</html>");
+        return result.toString();
+    }
+}
+
